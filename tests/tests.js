3c98d79fe11d5237b307235b3a25c829d692aace
Brandon DeRosier <bdero@mit.edu>
2015-04-19 14:37:59 -0400

    Add basic construction tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,1 +1,17 @@
-// ..Tests to be added
+QUnit.module('basic construction', {
+  beforeEach: function() {
+    this.canvas = $('<canvas>')[0];
+  }
+});
+
+QUnit.test('environment checks', function(assert) {
+  assert.strictEqual(S.name, Sur.name, 'Sur and S have the same name');
+});
+
+QUnit.test('construct Sur object with initShaders=false', function(assert) {
+  this.sur = new Sur(this.canvas, initShaders=false);
+
+  assert.ok(_.isObject(this.sur), 'object returned from Sur');
+  assert.ok(this.sur.hasOwnProperty('shaderSets'), 'has shaderSets property');
+  assert.propEqual(this.sur.shaderSets, {}, 'sur.shaderSets is empty');
+});
