05fdabd10a4519a397f943bfba797c140c006131
Brandon DeRosier <bdero@mit.edu>
2015-04-29 18:45:50 -0400

    Update license header to GNU GPL v3

--- sur.js
+++ sur.js
@@ -1,8 +1,20 @@
 /**
  * @license
- * sur 0.0.0 <https://github.com/bdero/sur/>
- * Copyright 2015 Brandon DeRosier
- * Available under MIT license <https://github.com/bdero/sur/blob/master/LICENSE.md>
+ * sur 0.1.0 <https://github.com/bdero/sur/>
+ * Copyright (C) 2015  Brandon DeRosier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * [GNU General Public License](LICENSE.md) for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 ;(function() {
 
